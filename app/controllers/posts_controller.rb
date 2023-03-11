6e961e53abe70f9f0c235ac3b941b127ef9991d3
trevor dewindt <trevor.dewindt@gmail.com>
2023-03-10 17:38:20 -0700

    commit tree e8b6c2dc6a12157b4a6723b285a433629dd8a60a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,15 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_post_data_response
+
 
   def show
-    post = Post.find(params[:id])
-    
+    post = find_posts
     render json: post
   end
 
   def update
-    post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post = find_posts
+    post.update!(post_params)
     render json: post
   end
 
@@ -18,6 +17,14 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def find_posts
+    Post.find(params[:id])
+  end
+
+  def unprocessable_post_data_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
